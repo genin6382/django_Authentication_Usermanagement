b6d255a5d367befb70d5b99cbdbf7d3d18ba35a7
Vidhu <100513712+genin6382@users.noreply.github.com>
2024-06-17 15:22:49 +0530

    Update admin.py

--- Usermanagement/myapp/admin.py
+++ Usermanagement/myapp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from .models import Post
+from django.contrib.admin.sites import AlreadyRegistered
+
 # Register your models here.
-admin.site.register(User)
+try:
+    admin.site.register(User)
+except AlreadyRegistered:
+    pass
+
 admin.site.register(Post)
